d51a9e74d3f7f2bb27319d6e34d9937b6f9477c6
zoe <test@com.tw>
2019-09-24 22:44:25 +0800

    cursors

--- Scene1.js
+++ Scene1.js
@@ -16,7 +16,6 @@ class Scene1 extends Phaser.Scene {
     }
     preload() {
         this.load.image('bg', 'assets/background.png');
-        this.load.image('ground', 'assets/platform.png');
         this.load.image('obstacle1', 'assets/obstacle1.png');
         this.load.image('obstacle2', 'assets/obstacle2.png');
         this.load.image('obstacle3', 'assets/obstacle3.png');
@@ -30,18 +29,18 @@ class Scene1 extends Phaser.Scene {
         this.tileSprite = this.add.tileSprite(0, 0, 1600, 800, "bg");
         //沒有狀態，不需控制的物件，如 obstacle3s，一般直接
         //在scene裏面建立就好
-        this.obstacle3s = this.physics.add.staticGroup();
-        this.obstacle3s.create(500, 150, 'obstacle3').setScale(0.4).refreshBody();
-        this.obstacle3s.create(1000, 350, 'obstacle3').setScale(0.3).refreshBody();
-        this.obstacle3s.create(1200, 100, 'obstacle3').setScale(0.4).refreshBody();
-        this.obstacle3s.create(900, 350, 'obstacle3').setScale(0.3).refreshBody();
 
         this.obstacle1s = this.physics.add.group({
-            key: 'obstacle1', repeat: 50, setXY: { x: 50, y: 100, stepX: 750, stepY: 40 }
+            key: 'obstacle1', repeat: 50, setXY: { x: 50, y: 100, stepX: 750, stepY: 2 }
         });
         this.obstacle2s = this.physics.add.group({
-            key: 'obstacle2', repeat: 50, setXY: { x: 200, y: 350, stepX: 800, stepY: -100 }
+            key: 'obstacle2', repeat: 50, setXY: { x: 200, y: 350, stepX: 800 }
         });
+        this.obstacle3s = this.physics.add.group({
+            key: 'obstacle3', repeat: 50, setXY: { x: 150, y: 200, stepX: 800 }
+        });
+
+        this.player = new Player(this, 60, 350);
 
         this.anims.create({
             key: 'gameover',
@@ -56,15 +55,16 @@ class Scene1 extends Phaser.Scene {
         });
 
         //主角、怪物等等通常會抽出去
-        this.player = new Player(this, 60, 350);
+        
         //this.player.setGravityX(100);
+
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.physics.add.overlap(this.player, this.obstacle1s, this.collectStar, null, this);
-        this.physics.add.overlap(this.player, this.obstacle2s, this.collectStar, null, this);
-        this.physics.add.overlap(this.player, this.obstacle3s, this.collectStar, null, this);
+        this.physics.add.overlap(this.player, this.obstacle1s, this.collision, null, this);
+        this.physics.add.overlap(this.player, this.obstacle2s, this.collision, null, this);
+        this.physics.add.overlap(this.player, this.obstacle3s, this.collision, null, this);
     }
 
-    collectStar(player, obstacle) {
+    collision(player, obstacle) {
         obstacle.disableBody(true, true);
         this.animationKey="gameover";
     }
@@ -73,6 +73,7 @@ class Scene1 extends Phaser.Scene {
         this.tileSprite.tilePositionX += 5;
         this.obstacle1s.setVelocityX(-100);
         this.obstacle2s.setVelocityX(-200);
+        this.obstacle3s.setVelocityX(-200);
 
         if (this.cursors.up.isDown) {
             this.player.setVelocityY(-100);
@@ -81,7 +82,6 @@ class Scene1 extends Phaser.Scene {
             this.player.setVelocityY(100);
             this.player.anims.play(this.animationKey, true);
         } else {
-            this.player.setVelocityX(0);
             this.player.anims.play(this.animationKey, true);
         }
     }
